dd97fa9f5865784866612fc087ce306cfeeb78dc
Philip Suskin <philip.suskin@classix.de>
2024-02-09 18:51:34 +0100

    May be getting somewhere

--- static/chat.js
+++ static/chat.js
@@ -1,3 +1,13 @@
+let mediaRecorder;
+let stream;
+navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(mediaStream => {
+        stream = mediaStream;
+    })
+    .catch(error => {
+        console.error('Error accessing microphone:', error);
+    });
+
 function chatBot() {
     return {
         botTyping: false,
@@ -5,14 +15,9 @@ function chatBot() {
             from: 'bot',
             text: 'Wie kann ich Ihnen behilflich sein?'
         }],
+        audioChunks: [],
+        isRecording: false,
         output: function(input) {
-            let product = 'Cool!';
-
-            // Update DOM
-            this.addChat(input, product);
-        },
-        addChat: function(input, product) {
-
             // Add user message
             this.messages.push({
                 from: 'user',
@@ -26,7 +31,9 @@ function chatBot() {
             setTimeout(() => {
                 this.botTyping = true;
                 this.scrollChat();
-            }, 1000)
+            }, 500)
+
+            let product = 'Cool!';
 
             // add bit message with Fake delay to seem "real"
             setTimeout(() => {
@@ -37,7 +44,6 @@ function chatBot() {
                 });
                 this.scrollChat();
             }, ((product.length / 10) * 1000) + (Math.floor(Math.random() * 2000) + 1500))
-
         },
         scrollChat: function() {
             const messagesContainer = document.getElementById("messages");
@@ -51,6 +57,77 @@ function chatBot() {
                 this.output(target.value.trim());
                 target.value = '';
             }
+        },
+        record: function() {
+            if (!this.isRecording) {
+                this.startRecording();
+            } else {
+                this.stopRecording();
+            }
+        },
+        startRecording: function() {
+            // audioChunks = []; // Clear previous audio chunks
+            // mediaRecorder = new MediaRecorder(stream);
+        
+            // mediaRecorder.addEventListener('dataavailable', event => {
+            //     audioChunks.push(event.data);
+            // });
+        
+            // mediaRecorder.start();
+            this.isRecording = true;
+            document.getElementById('micIcon').classList.remove("fa-microphone");
+            document.getElementById('micIcon').classList.add("fa-microphone-slash");
+            document.getElementById('recordButton').classList.add('recording');
+            console.log('Recording started');
+        },
+        stopRecording: function() {
+            // mediaRecorder.stop();
+            this.isRecording = false;
+            document.getElementById('micIcon').classList.remove("fa-microphone-slash");
+            document.getElementById('micIcon').classList.add("fa-microphone");
+            document.getElementById('recordButton').classList.remove('recording');
+            console.log('Recording ended');
+        
+            // Create a new Blob containing the recorded audio
+            const audioBlob = new Blob(this.audioChunks, { type: 'audio/chunks[0].type' });
+        
+            // Create FormData object to send audio file to server
+            const formData = new FormData();
+            formData.append('audio', audioBlob);
+
+            let self = this;
+
+            // Send POST request to server
+            $.ajax({
+                type: 'POST',
+                url: '/',
+                contentType: 'application/json',
+                data: JSON.stringify({"text": "test"}),
+                success: function(response) {
+                    console.log(response["response"]);
+                    self.output(response["response"]);
+                    this.audioChunks = [];
+                },
+                error: function(error) {
+                    console.error(error);
+                }
+            });
+        
+            // $.ajax({
+            //     type: 'POST',
+            //     url: '/',
+            //     processData: false,
+            //     contentType: false,
+            //     data: formData,
+            //     success: function(response) {},
+            //     error: function(error) {
+            //         console.error(error);
+            //     }
+            // });
         }
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const chatBotInstance = new ChatBot();
+});
